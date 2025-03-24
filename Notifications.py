01dfdde94c68e406b947bc81a4bcb5e4c4b8d88d
Abby-Josh <abby.joshowitz@koheletyeshiva.org>
2025-03-24 16:26:06 +0000

    comments and new json

--- Notifications.py
+++ Notifications.py
@@ -1,3 +1,4 @@
+
 import json
 
 def read_json_to_dict(file_path):
@@ -22,45 +23,58 @@ def read_json_to_dict(file_path):
         return None
 
 
-# Example usage:
+# Load our json file and turn it into a dictionary:
 file_path = 'backroundinfo.json'
 data_dict = read_json_to_dict(file_path)
+happening_path = 'Happening.json'
+happening_dict = read_json_to_dict(happening_path)
 
-#if data_dict:
-    #print("JSON data as dictionary:")
-    #print(data_dict)
+def notify(happenings, data):
+    """
+    This is meant to notify the user when they enter a new location information on the Jews in that location. 
 
+    Args:
+        data (dict): The dictionary created from the JSON file.
 
-def notify(data):
-    #This is when you input your location  
+    """
+    #For the time being we have the user enter their location manually   
     user_location = input ("Where are you?")
     print ("You are in ", user_location)
-    if (user_location == "Lithuania"):
-        print("Happenings-  Community Bible Studies and Purim Party")
-    if (user_location == "Calgary"):
-        print("Happenings- Walk for Israel 3/25/25")
-    
 
-    # Once you put in your location this will give you information based on where you are
-    for i in data: #looping through list
+    #if (user_location == "Lithuania"):
+        #print("Happenings-  Community Bible Studies and Purim Party")
+    #if (user_location == "Calgary"):
+        #print("Happenings- Walk for Israel 3/25/25")
+    
+    for i in happenings: #looping through list
         for key, value in i.items(): #looping through top dict
             #print (key, value)
             if key == user_location:
                 for innerkey, innervalue in i[key].items():
                     #print (innerkey, innervalue)
                     print (f"{innerkey} - {innervalue}")
+            
+
+    # Once you put in your location this will give you information based on where you are
+    for i in data: #looping through list
+        for key, value in i.items(): #looping through top dict
+            #print (key, value)
+            if key == user_location:
+                for innerkey, innervalue in i[key].items(): #looping through inner dictionary
+                    #print (innerkey, innervalue)
+                    print (f"{innerkey} - {innervalue}")
                 repeat=input("try again? y or n")
                 if (repeat=="y"):
-                    notify(data)
+                    notify(happenings, data) #recursively call the notify function to run it again
                 else:
                     exit()
             
             #else: 
     print("We don't have information on that, try again")
-    notify(data)
+    notify(happenings, data) #recursively call the notify function to run it again
 
 
-notify(data_dict)
+notify(happening_dict, data_dict)
             
 
             
